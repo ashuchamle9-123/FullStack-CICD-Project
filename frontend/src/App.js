53ff7f45e71d9e3a61c1c65f44b3882b77a68fc1
root <root@ip-172-31-10-228.ap-northeast-1.compute.internal>
2026-05-26 16:52:34 +0000

    Fixed submodule issue

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>Full Stack CI/CD Pipeline Project</h1>
+      <h2>Frontend Running Successfully</h2>
+    </div>
+  );
+}
+
+export default App;
